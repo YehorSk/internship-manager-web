3c05e641edcdfda00ea9b7a5bcf433ee26545171
YehorSk <95549799+YehorSk@users.noreply.github.com>
2025-09-26 11:36:29 +0200

    env variables

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,25 @@
 import { fileURLToPath, URL } from 'node:url'
 
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    },
-  },
+// https://vitejs.dev/config/
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+  return {
+    plugins: [
+      vue(),
+    ],
+    resolve:
+      {
+        alias: {
+          '@': fileURLToPath(new URL('./src', import.meta.url))
+        }
+      },
+    server: {
+      allowedHosts: [env.VITE_API_BASE_URL.replace(/^https?:\/\//, ''), 'localhost'],
+      host: '0.0.0.0',
+      port: 3000,
+    }
+  }
 })
