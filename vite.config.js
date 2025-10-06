dbde3fa981c0e5a7b6c535e35e402f977b14f86e
RomanVasylyk <131609378+RomanVasylyk@users.noreply.github.com>
2025-10-06 15:49:23 +0200

    Add axios and vuetify dependencies

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import vuetify from 'vite-plugin-vuetify'
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
@@ -9,6 +10,7 @@ export default defineConfig(({ mode }) => {
   return {
     plugins: [
       vue(),
+      vuetify(),
     ],
     resolve:
       {
